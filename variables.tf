923b107eeecce8c9a59be839166a3bbb8bd4fc5f
maximillianbrain1 <maxvbrain@gmail.com>
2020-03-02 17:31:41 -0500

    Circleci test

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "wifi_name" { default = "WIFI!" }
 
 variable "wifi_password" { default = "PASSWORD"  }
 
-variable "ssh_key_path" { default = "./README.md" }
+variable "ssh_key_path" { default = "./id_rsa" }
 
-variable "master_ip" { default = "127.0.0.1"  }
+variable "master_ip" { default = "127.0.0.1:6443"  }
 
 variable "k3s_token" { default = "TOKEN" }
