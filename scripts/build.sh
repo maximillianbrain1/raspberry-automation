45b2f33712d05614adcb909683ebdcdf31c12d5a
maximillianbrain1 <maxvbrain@gmail.com>
2020-03-02 22:37:34 -0500

    Added new packer configs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ex
 
-echo $PWD
-ls
-ls scripts/
-
-scripts/setup_ssh.sh
-scripts/setup_wifi.sh
-scripts/setup_k3s.sh
+/scripts/setup_ssh.sh
+/scripts/setup_wifi.sh
+/scripts/setup_k3s.sh
